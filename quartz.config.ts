154dd5cb92a4d9269936c021ac828754c8c6833f
molusaddsa <lmj77877@gmail.com>
2026-05-07 13:50:47 +0900

    옵시디언으로 이름 변경

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "이러나 블로그",
+    pageTitle: "이러나 옵시디언",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
